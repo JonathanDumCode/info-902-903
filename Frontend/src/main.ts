943b43d8e5e9049c256b7fe930afecaa58709a84
Tauste73 <77971546+Tauste73@users.noreply.github.com>
2024-03-08 14:38:18 +0100

    feat: add vuetify

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -4,8 +4,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
 
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+const app = createApp(App)
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
