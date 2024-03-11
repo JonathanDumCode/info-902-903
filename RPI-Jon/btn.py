fec24e6f6f7bbd9528b8ce29ef3d4f99d2853801
Jonathan Dumont <jonathan.dumont.info@gmail.com>
2024-03-11 13:52:11 +0100

    btn

--- RPI-Jon/btn.py
+++ RPI-Jon/btn.py
@@ -1,9 +1,14 @@
-from gpiozero import Button
+import RPi.GPIO as GPIO
+from time import sleep
 
-button = Button(21)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+i = 0
 while True:
-    if button.is_pressed:
+    if GPIO.input(5) == GPIO.HIGH:
         print("ON")
     else:
         print("OFF")
+    sleep(0.5)
